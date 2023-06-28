6478c83dc0781aff86c4adb09d74a563705e5d34
Arthur Rodrigues <arthur.rodrigues@varsolutions.com.br>
2023-06-28 14:51:07 -0300

    Alterei o estilo do site e reorganizei o codigo.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function Feedback() {
+    var Comentario = document.getElementById("Comentario").value
+    if (Comentario != "") {
+        alert('Obrigado Pelo Feedback!')
+    }
+    else 
+    {
+        return false;
+    }
+    
+   
+}
